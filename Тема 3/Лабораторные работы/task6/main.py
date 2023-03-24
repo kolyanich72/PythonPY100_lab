70df6aac171cc812e008f360555d2db91a353e17
kolyanich72 <kolyanich72@gmail.com>
2023-03-24 15:25:37 +0300

    task2

--- Тема 3/Лабораторные работы/task6/main.py
+++ Тема 3/Лабораторные работы/task6/main.py
@@ -1,5 +1,8 @@
 list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO Оформить решение
-
+a = max(list_numbers)
+i = list_numbers.index(a)
+list_numbers.insert(i, list_numbers.pop(-1))
+list_numbers.append(list_numbers.pop(i+1))
 print(list_numbers)
--- Тема 3/Лабораторные работы/task9/main.py
+++ Тема 3/Лабораторные работы/task9/main.py
@@ -6,5 +6,9 @@ increase = 0.03  # рост цен
 need_money = 0  # количество денег, чтобы прожить 10 месяцев
 
 # TODO Оформить решение
+for i in range(1,11):
+    if i > 1:
+        spend *= (1 + increase)
+    need_money += spend - salary
 
 print(round(need_money))
